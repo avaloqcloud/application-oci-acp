6f3a4e8365e4c456501ffac8ac1d3752ce2ab88a
Sagar Chopade <Sagar.Chopade@avaloq.com>
2023-10-09 17:35:43 +0530

    Used acf-compute module to create an instnce

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 
 module "acf_resource_compute" {
-    source = "https://github.com/avaloqcloud/acf_resource_compute"
+    source = "https://github.com/avaloqcloud/acf_resource_compute.git"
     compartment_ocid        = var.compartment_ocid
     cloud_init_script       = "${file("${path.module}/scripts/config.sh")}"
     subnet_id               = var.private_subnet_id
