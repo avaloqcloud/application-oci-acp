433b95552bddc37beaea1314777eb2928c4c0b73
Tejas Avhad <Tejas.Avhad@avaloq.com>
2024-01-10 15:14:56 +0530

    added ref

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "acp" {
-    source = "github.com/avaloqcloud/terraform-oci-compute?ref=bb95ffff36c5c771b21bd8795c5556b2515167c3"
+    source = "github.com/avaloqcloud/terraform-oci-compute?ref=b95ffff36c5c771b21bd8795c5556b2515167c3"
     compartment_ocid        = var.compartment_ocid
     cloud_init_script       = "${file("${path.module}/scripts/config.sh")}"
     subnet_id               = var.private_subnet_id
