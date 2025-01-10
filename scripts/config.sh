0baa988f92a64c27ea06f384d164aff549327843
TejasAvhad1999 <155512535+TejasAvhad1999@users.noreply.github.com>
2025-01-10 10:12:14 +0530

    Update acp scripts

--- scripts/config.sh
+++ scripts/config.sh
@@ -16,7 +16,8 @@ sed -i "s/\(HOST=\s*\)[0-9a-z\.]\+\(.*\)/\1${escaped_ip}\2/" "${listener_file_pa
 sed -i "s/\(Host\s*=\s*\)[0-9a-z\.]\+\(.*\)/\1${escaped_ip}\2/" "${tnsnames_file_path}"
 
 # SID Name
-sid_name=$(grep -E  -o "SID_NAME= [a-z0-9]+" "${listener_file_path}" | sed -r "s/SID_NAME= //")
+sid_name=$(hostname)
+echo "$new_ip $sid_name.swprod1.migration.oraclevcn.com  $sid_name" >> /etc/hosts
 
 su - oracle << RUN_AS_ORACLE
 /home/oracle/aaa/bin/aaadb.ksh -s "${sid_name}" -c restart
